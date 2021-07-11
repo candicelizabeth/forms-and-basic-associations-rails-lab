2bc10cef99268097b876e12301bf2d5e784201df
Candice Peters <candice.peters@flatironschool.com>
2021-07-11 01:29:27 -0500

    commit tree f48378c344778ce1b093627e6fe7584fc2aef489

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,27 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+  
+
+  def artist_name=(name)
+    if name != ""
+      self.artist = Artist.find_or_create_by(name: name)
+    end
+  end
+
+  def artist_name
+    # byebug
+    self.artist ? self.artist.name : nil
+  end
+
+  def notes_content=(contents)
+    contents.each do |c| 
+      if !c.blank? 
+        self.notes.build(content: c)
+      end
+    end
+    # byebug
+  end
 end
